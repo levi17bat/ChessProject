1b640bf8bf4eaeb43d892248aee8df60143480d8
Levi <levibatista3@gmail.com>
2021-02-03 22:15:58 -0300

    Handling exceptions and clearing screen

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -3,7 +3,9 @@ package Application;
 import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
+import Chess.Exceptions.ChessException;
 import boardGame.Board;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -16,13 +18,22 @@ public class Program {
         Scanner ler = new Scanner(System.in);
         ChessMatch jogo = new ChessMatch();
         while (true) {
-            UI.printBoard(jogo.getPieces());
-            System.out.print("\nSource = ");
-            ChessPosition source = UI.readChessPosition(ler);
-            System.out.print("\nTarget = ");
-            ChessPosition target = UI.readChessPosition(ler);
-            ChessPiece capturedPiece = jogo.performChessMove(source,target);
-            
+            try {
+                UI.clearScreen();
+                UI.printBoard(jogo.getPieces());
+                System.out.print("\nSource = ");
+                ChessPosition source = UI.readChessPosition(ler);
+                System.out.print("\nTarget = ");
+                ChessPosition target = UI.readChessPosition(ler);
+                ChessPiece capturedPiece = jogo.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+            }
+
         }
 
     }
--- src/Chess/Pieces/King.java
+++ src/Chess/Pieces/King.java
@@ -13,6 +13,7 @@ public class King extends ChessPiece{
     
     public King(Board board, COLOR color) {
         super(board,color);
+        
     }
     
     @Override
